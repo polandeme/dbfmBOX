359e3e75ba363a7c9f52cd458fe9009f94adef87
polandeme <polandeme@gmail.com>
2014-10-21 13:03:50 +0800

    write to chrome

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-// window.onload = function() {
-	jk('#btn').click(function() {
-		console.log('dts');
+// jk('#btn').click(function() {
+		// var song = jk('#song').value;
+		// var song = document.getElementById('song').value;
 		jk().ajax({
-			url: 'http://geci.me/api/lyric/枫',
+			url: 'http://geci.me/api/lyric/' + 'dd',
 			type: 'GET',
 			success: function(data) {
-				console.log(data);
+					if(data.result.length > 0) {
+						jk().ajax({
+							url: data.result[0].lrc,
+							type: 'GET',
+							success: function(data) {
+								console.log(data);
+								var text = encode_lrc(data);
+								jk('#lrc-box').html(text);
+							},
+							// error status
+							error: function(err) { 
+								console.log('err');
+							}
+						});
+					} else {
+						console.log('没有找到歌词');
+					}
 			},
-			error: function(err) {
-				console.log('dds');
+			// error status
+			error: function(err) { 
 			}
 		});
-	})
-	
-// }
+
+	// 处理得到的歌词
+	function encode_lrc(text) {
+		var s = text.replace(/\[(.*)\]/g, '').trim();//去除返回数据的[]两端的内容，只保留歌词部分  
+    	return s.replace(/\n/g, '\n<br />');//每行末尾输出html的换行符  
+	}
+// });
