bf60dbe2b21d71861a2fe31bd7294636fb427970
polandeme <polandeme@gmail.com>
2014-10-29 21:32:07 +0800

    fixed selected

--- jk.js
+++ jk.js
@@ -232,4 +232,3 @@
 
     window.$ = window.jk = jk;
 })();
-
--- style.js
+++ style.js
@@ -14,13 +14,41 @@ var body = document.querySelectorAll('body')[0];
 var cname = document.querySelectorAll('.cname');
 var title = [];//hz title
 var href = [];
+
 var firstChild = document.querySelectorAll('#fm-section')[0];
-for(var i = 0; i < cname.length; i++) {
-	console.log(cname[i]);
-	//.firstChild.nodeVlaue);
-	// body.insertBefore(oldNode_A[i], firstChild[0]);
-	// console.log(i);
+var cwarp = document.createElement('div');	
+var cnameNode = '';
 
-}
+// for(var i = 0; i < cname.length-2; i++) {
+// 	title.push(cname[i].firstChild.data);
+// 	cnameNode += "<li class='channel' style='float: left;><a class='chl_name'> <span class='cname' '>"+ cname[i].firstChild.data +"</li></span></a>";
+// }
+// console.log(cnameNode);
+// function cet(cwarp, childNode) {
+// 	cwarp.id = 'cwarp';
+// 	console.log(childNode);
+// 	cwarp.innerHTML = childNode;
+// 	return cwarp;
+// }
+// console.log(cet(cwarp, cnameNode));
+// body.insertBefore(cet(cwarp, cnameNode), firstChild);
 
+// jk('.cname').css({
+// 	'margin': '5px'
+// });
 
+// jk('#cwarp').css({
+// 	'width': '100px',
+// 	'marginLeft': '30px'
+// });
+jk('.hidden_list').remove();
+jk('.chl_name').css({
+	'width':'100px'
+});
+jk('.channel ').css({
+	'marginBottom': '0px'
+});
+//
+jk('.channel_list').css({
+	'width': '100px',
+});
